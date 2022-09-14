44c45007550bf76983b15c17cd934ede2b946161
koustubh muktibodh <koustub28@hotmail.com>
2022-09-14 16:00:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function showMenu() {
+  var m = document.querySelector(".menu");
+  m.classList.add("smenu");
+}
+
+function closeMenu() {
+  var m = document.querySelector(".menu");
+  m.classList.remove("smenu");
+}
